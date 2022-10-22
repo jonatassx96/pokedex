c798b4ccb574d5d4cd182b2982b74fc9c112b27a
Jonatas Xavier <jonatas.sx96@gmail.com>
2022-10-22 01:00:06 -0300

    first get

---
+++ assets/scripts/main.js
@@ -0,0 +1,10 @@
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+//Processamento assincrono
+fetch(url)
+  .then((response) => response.json())
+  .then((jsonBody) => console.log(jsonBody))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Requisição comcluida."))
