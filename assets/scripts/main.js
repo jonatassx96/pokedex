02f33f91256c9f3f685bea37e03b53499d00a0b1
Jonatas Xavier <81437599+jonatassx96@users.noreply.github.com>
2022-10-24 13:46:58 -0300

    feat: add function convertPokemonToLi(pokemon)
    Adicionado a função que cria a lista de pokemon dinamicamente com o consumo do pokeapi.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,9 +2,34 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+  return `
+    <li class="pokemon">
+      <span class="number-pokemon">#001</span>
+      <span class="name">${pokemon.name}</span>
+      <div class="detail">
+        <ol class="types">
+          <li class="type">grass</li>
+          <li class="type">poison</li>
+        </ol>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+          alt="${pokemon.name}"
+        />
+      </div>
+    </li>
+  `
+}
+
+const pokemonOl = document.getElementById('pokemonList')
 //Processamento assincrono
 fetch(url)
   .then((response) => response.json())
-  .then((jsonBody) => console.log(jsonBody))
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemonList) => {
+    for (let i = 1; i < pokemonList.length; i++){
+      const pokemon = pokemonList[i]
+      pokemonOl.innerHTML += convertPokemonToLi(pokemon)
+    }
+  })
   .catch((error) => console.log(error))
-  .finally(() => console.log("Requisição comcluida."))
