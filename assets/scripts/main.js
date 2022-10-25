e1bab8b6780361a29d7a5b55896bdb0033ae5265
Jonatas Xavier <81437599+jonatassx96@users.noreply.github.com>
2022-10-25 20:35:17 -0300

    feat:
    feat:

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,27 +1,41 @@
 
-function convertPokemonTypesToLi(pokemonTypes) {
-  return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
 
+//Deixando a primeira letra maiuscula
 function convertPokemonNameUpperCase(pokemonName) {
   const pokemonNameLowerCase = pokemonName
   const pokemonUpperCase = pokemonNameLowerCase[0].toUpperCase() + pokemonNameLowerCase.substr(1);
   return pokemonUpperCase
 }
+//Deixando id do pokemon com 3 digitos
+function idThreeDigits(idPokemon) {
+  let idTrhee = idPokemon
+  if (idTrhee <= 9) {
+    idTrhee = `00${idTrhee}`
+  } else if (idTrhee <= 99) {
+    idTrhee = `0${idTrhee}`
+  } else if (idTrhee > 99) {
+    idTrhee = `${idTrhee}`
+  }
+  return idTrhee
+}
+
 
 function convertPokemonToLi(pokemon) {
   return `
-    <li class="pokemon">
-      <span class="number-pokemon">#${pokemon.order}</span>
+    <li class="pokemon ${pokemon.type}">
+      <span class="number-pokemon">#${idThreeDigits(pokemon.id)}</span>
       <span class="name">${convertPokemonNameUpperCase(pokemon.name)}</span>
       <div class="detail">
         <ol class="types">
-          ${convertPokemonTypesToLi(pokemon.types).join('')}
+          ${pokemon.types.map((type) => `<li class="type btn-${type}">${type}</li>`).join('')}
         </ol>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-          alt="${pokemon.name}"
-        />
+          <img  class="img-pokemon"
+            src="${pokemon.photo}"
+            alt="${pokemon.name}"
+          />
+          <img src="./assets/imagens/pokeball.png" class="img-bg-pokemon"
+          alt="fundo"
+          />
       </div>
     </li>
   `
--- assets/scripts/poke-api.js
+++ assets/scripts/poke-api.js
@@ -1,19 +1,35 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemonsDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.id = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.front_default
+
+    return pokemon
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 20) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 100) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
-            .then((detailRequests) => Promise.all(detailRequests))
-            .then((pokemonsDetails) => pokemonsDetails)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
-
-//{console.log(pokemonsDetails)})
