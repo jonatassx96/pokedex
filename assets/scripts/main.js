4d8daab361f6abdd045892a7c1f5ac724abc8125
Jonatas Xavier <81437599+jonatassx96@users.noreply.github.com>
2022-10-26 16:20:48 -0300

    Add all pokemons

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,7 +4,7 @@ const moreButton = document.getElementById("more")
 const newPagePokemon = document.getElementsByClassName("pokemon")
 const limit = 60;
 let offset = 0;
-const maxRecords = 251
+const maxRecords = 2000
 
 
 //Deixando a primeira letra maiuscula
