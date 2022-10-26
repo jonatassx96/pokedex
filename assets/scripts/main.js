9df02972e9c44c2ba0ba85e42f511955b0cf0300
jonatassx96 <jonatas.sx96@gmail.com>
2022-10-26 02:29:53 -0300

    feat: add sprit animated

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,22 @@
-
-
 //Deixando a primeira letra maiuscula
 function convertPokemonNameUpperCase(pokemonName) {
-  const pokemonNameLowerCase = pokemonName
-  const pokemonUpperCase = pokemonNameLowerCase[0].toUpperCase() + pokemonNameLowerCase.substr(1);
-  return pokemonUpperCase
+  const pokemonNameLowerCase = pokemonName;
+  const pokemonUpperCase =
+    pokemonNameLowerCase[0].toUpperCase() + pokemonNameLowerCase.substr(1);
+  return pokemonUpperCase;
 }
 //Deixando id do pokemon com 3 digitos
 function idThreeDigits(idPokemon) {
-  let idTrhee = idPokemon
+  let idTrhee = idPokemon;
   if (idTrhee <= 9) {
-    idTrhee = `00${idTrhee}`
+    idTrhee = `00${idTrhee}`;
   } else if (idTrhee <= 99) {
-    idTrhee = `0${idTrhee}`
+    idTrhee = `0${idTrhee}`;
   } else if (idTrhee > 99) {
-    idTrhee = `${idTrhee}`
+    idTrhee = `${idTrhee}`;
   }
-  return idTrhee
+  return idTrhee;
 }
-
 
 function convertPokemonToLi(pokemon) {
   return `
@@ -27,23 +25,27 @@ function convertPokemonToLi(pokemon) {
       <span class="name">${convertPokemonNameUpperCase(pokemon.name)}</span>
       <div class="detail">
         <ol class="types">
-          ${pokemon.types.map((type) => `<li class="type btn-${type}">${type}</li>`).join('')}
+          ${pokemon.types
+            .map((type) => `<li class="type btn-${type}">${type}</li>`)
+            .join("")}
         </ol>
-          <img  class="img-pokemon"
-            src="${pokemon.photo}"
-            alt="${pokemon.name}"
-          />
+          <div class="container-img">
+            <img  class="img-pokemon"
+              src="${pokemon.photo}"
+              alt="${pokemon.name}"
+            />
+          </div>
           <img src="./assets/imagens/pokeball.png" class="img-bg-pokemon"
           alt="fundo"
           />
       </div>
     </li>
-  `
+  `;
 }
 
-const pokemonOl = document.getElementById('pokemonList')
+const pokemonOl = document.getElementById("pokemonList");
 //Processamento assincrono
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonOl.innerHTML += pokemons.map(convertPokemonToLi).join('')
-})
+  pokemonOl.innerHTML += pokemons.map(convertPokemonToLi).join("");
+});
